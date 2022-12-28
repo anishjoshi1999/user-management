000adbe7c0d26b50d2b6d4956ebf24e7eacfaae3
anishjoshi1999 <anishjoshi1999@gmail.com>
2022-12-28 17:22:18 +0545

    modification

--- index.js
+++ index.js
@@ -1,87 +1,87 @@
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config()
+}
+const methodOverride = require('method-override')
 const express = require('express')
 const app = express()
-const path = require('path')
-const querystring = require('querystring')
-const ejsMate = require('ejs-mate')
-const dotenv = require("dotenv").config()
-const mongoose = require('mongoose')
-var bodyParser = require('body-parser')
-const paginatedResults = require('./middleware/pagination')
-const methodOverride = require('method-override')
-const userManagement = require('./models/userManagement')
-const conn_str = `mongodb+srv://anishjoshi2056:${process.env.MONGODB_ATLAS_PASSWORD}@cluster0.mfsduzy.mongodb.net/?retryWrites=true&w=majority`
-mongoose.connect(conn_str)
-    .then(() => {
-        console.log("connection open")
-    })
-    .catch((err) => {
-        console.log("error found")
-        console.log(err)
-    })
-app.engine('ejs', ejsMate)
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-// override with POST having ?_method=DELETE
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const initializePassport = require('./passportConfig')
+const flash = require('express-flash')
+const session = require('express-session')
+
+initializePassport(passport, email => {
+    return users.find(user => user.email === email)
+},
+    id => {
+        return users.find(user => user.id === id)
+    }
+)
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false
+}))
 app.use(methodOverride('_method'))
 
-// Routes
-// Displaying all the user name
-app.get("/", (req, res) => {
-    const query = querystring.stringify({ page: '1' });
-    res.redirect(`/users?${query}&limit=10`);
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', checkAuthenticated, (req, res) => {
+    res.render('index.ejs', { name: req.user.name })
 })
-
-
-// To show all the users inside the database
-app.get('/users',paginatedResults(userManagement),async (req, res) => {
-    const {next,previous,users,count} = res.paginatedResults
-    let totalPages;
-    if(!Number.isInteger(count/10)){
-        totalPages = Math.floor(count/10) + 1
-    }else {
-        totalPages = Math.floor(count/10) 
+app.get('/login', checkNotAuthenticated, (req, res) => {
+    res.render('login.ejs')
+})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+app.get('/register', checkNotAuthenticated, (req, res) => {
+    res.render('register.ejs')
+})
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        console.log(hashedPassword)
+        users.push({
+            id: Date.now().toString(),
+            name: name,
+            email: email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch (error) {
+        res.redirect('/register')
     }
-    res.render('UM/index', { users ,previous,next,totalPages})
+    console.log(users)
 })
-// To create a new user info
-app.get('/users/new', async (req, res) => {
-    res.render('UM/new')
+
+app.delete('/logout', (req, res) => {
+    req.logOut()
+    res.redirect('/login')
 })
-// To see the info of a specfic user
-app.post('/users', async (req, res) => {
-    const newUser = new userManagement(req.body)
-    await newUser.save(() => {
-        console.log("new user added successfully")
-    })
-    res.redirect('/users')
-})
-// To show a specific user
-app.get('/users/:id', async (req, res) => {
-    const { id } = req.params
-    // Find user by id
-    const user = await userManagement.findById(id)
-    res.render('UM/show', { user })
-})
-// To show a edit form for a specific user
-app.get('/users/:id/edit', async (req, res) => {
-    const { id } = req.params
-    // Find user by id
-    const user = await userManagement.findById(id)
-    res.render('UM/edit', { user })
-})
-app.patch('/users/:id', async (req, res) => {
-    const { id } = req.params
-    await userManagement.findByIdAndUpdate(id, req.body)
-    res.redirect(`/users/${id}`)
-})
-// To delete a specific user
-app.delete('/users/:id', async (req, res) => {
-    const { id } = req.params;
-    await userManagement.findByIdAndDelete(id)
-    res.redirect('/users')
-})
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        next()
+    }
+    res.redirect('/login')
+}
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
+
+
 app.listen(3000, () => {
-    console.log("Serving on port 3000")
+    console.log("Listening to port 3000")
 })
